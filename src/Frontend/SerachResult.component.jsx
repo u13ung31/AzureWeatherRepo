d423312335b095201fd4a5924a08f5a4d10e9d92
Erik Engholm <45353988+Erikengholm@users.noreply.github.com>
2022-10-25 13:09:38 +0200

    Adding backend to search2

--- src/Frontend/SerachResult.component.jsx
+++ src/Frontend/SerachResult.component.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import { useLocation } from 'react-router-dom'
 import { SearchResultJS } from '../Backend/SearchResultQueryFinder';
-
+import ImportantString from "../Enum/ImportantStrings.json";
 const SearchResult = () =>{
     const[DataisLoaded,SetDataLoaded] = useState(false);
     const[items,SetItems] = useState([]);
@@ -19,7 +19,7 @@ const SearchResult = () =>{
             })*/
         return () => { isMounted = false };
     });
-
+    console.log(ImportantString);
     let checkIfSearchWork = true;
     var from = ""; 
     const location = useLocation()
