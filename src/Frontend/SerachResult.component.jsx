656085b001e67c09cf44c5d1186b665fec5aba6a
Erik Engholm <45353988+Erikengholm@users.noreply.github.com>
2022-10-25 13:54:59 +0200

    Added an HTTPTrigger

--- src/Frontend/SerachResult.component.jsx
+++ src/Frontend/SerachResult.component.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import { useLocation } from 'react-router-dom'
-import { SearchResultJS } from '../Backend/SearchResultQueryFinder';
+import { SearchResultJS } from '../Backend/Services/SearchResultQueryFinder';
 import ImportantString from "../Enum/ImportantStrings.json";
 const SearchResult = () =>{
     const[DataisLoaded,SetDataLoaded] = useState(false);
@@ -19,7 +19,6 @@ const SearchResult = () =>{
             })*/
         return () => { isMounted = false };
     });
-    console.log(ImportantString);
     let checkIfSearchWork = true;
     var from = ""; 
     const location = useLocation()
